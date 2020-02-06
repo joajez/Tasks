3b6449a7befa831295132df5ef01b98ef0bb223d
Joanna Jeziorek <asia.jeziorekk@gmail.com>
2020-02-06 12:49:54 +0100

    add validation

--- T2/script.js
+++ T2/script.js
@@ -33,23 +33,60 @@ class User {
 
 const button = document.getElementById("button");
 const details = document.querySelector("#details ul");
+const name = document.getElementById("name");
+const lastname = document.getElementById("lastName");
+button.disabled = true;
+let usersList = new Users();
+function checkValid() {
+  if (name.checkValidity() && lastname.checkValidity()) {
+    button.disabled = false;
+  } else {
+    button.disabled = true;
+  }
+}
+function clearFields() {
+  name.value = "";
+  lastname.value = "";
+  checkValid();
+}
 
-button.addEventListener("click", function () {
+name.addEventListener("input", checkValid);
+lastname.addEventListener("input", checkValid);
+
+button.addEventListener("click", function() {
+  details.innerHTML = "";
   const gender = document.querySelector('input[name="gender"]:checked').value;
   const user = new User(
     document.getElementById("name").value,
     document.getElementById("lastName").value,
     gender
   );
-  let usersList = new Users();
-  usersList.add(user);
   usersArray = usersList.get();
+  exist = usersArray.some(
+    e =>
+      e.firstName === user.firstName &&
+      e.lastName === user.lastName &&
+      e.gender === user.gender
+  );
 
+  try {
+    if (exist) {
+      throw "User already exists!";
+    } else {
+      usersList.add(user);
+      document.getElementById("error").innerHTML = "";
+    }
+  } catch (err) {
+    document.getElementById("error").innerHTML = "Error: " + err + ".";
+  }
+
+  usersArray = usersList.get();
   usersArray.map(writeDetails);
-
   function writeDetails(user) {
     details.innerHTML += `<li>${user.displayDetails()}</li>`;
-  };
+  }
+  clearFields();
+
   // To do:
   // create an object User
   // In the <div id="details"> display data from the form
@@ -76,3 +113,14 @@ button.addEventListener("click", function () {
 // - add created user to Users list
 // - display the list in <div id="details"> users.get() (tip: list is an array, you need to iterate)
 // You can play with html and CSS to get better view :)
+
+// TODO
+// 1. Disable "Add User" button
+// 2. Add simple validation:
+// - first name, last name - required fields, min 3 chars (only letters), please be aware about whitespaces
+// 3. Enable "Add User" button when first and last name fields mets expectations
+// 5. On "Add User" button:
+// - check if given user with given first, last name and gender already exist in the list
+// - if user doesn't exist in the list add it to the list and show (already done)
+// - if user exist in the list do not add it to the list and show error message (below form)
+// 4. After "Add user" button clicked clear fields value in the form
