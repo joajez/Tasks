8b1f118ad079b833f3741dec0ab24428e9acd1fc
Joanna Jeziorek <asia.jeziorekk@gmail.com>
2020-04-01 20:49:41 +0200

    refactor

--- T2/script.js
+++ T2/script.js
@@ -48,7 +48,6 @@ class User {
       usersList[usersList.length - 1] !== undefined
         ? usersList[usersList.length - 1].userId + 1
         : 1;
-    console.log(usersList);
   }
 
   // displayDetails() {
@@ -58,17 +57,17 @@ class User {
   // }
   displayDetails(id) {
     id = id + 1;
-    return `<tr>
-    <td class ="id${this.userId}">${id}</td>
+    return `<tr><td class ="id${this.userId}">${id}</td>
     <td class ="name${this.userId}" >${this.firstName}</td>
     <td class ="lastName${this.userId}">${this.lastName}</td>
     <td class ="gender${this.userId}">${this.gender}</td>
     <td class ="date${this.userId}" >${moment(this.creationDate).format(
       "DD/MM/YYYY HH:mm:ss"
     )}</td>
-    <td><button class="buttonEdit" id ="edit${
+    <td><button class="buttonEdit" id ="edit${this.userId}">Edit</button>
+    <button class="buttonSave hide" id ="save${
       this.userId
-    }">Edit</button><button class="buttonDelete" id ="delete${
+    }">Save</button><button class="buttonDelete" id ="delete${
       this.userId
     }" >Delete</button></td>
   </tr>`;
@@ -88,7 +87,9 @@ class User {
 // To do:
 // Show user details
 const button = document.getElementById("button");
-const buttonDelete = document.getElementsByClassName("buttonDelete");
+let buttonsDelete = document.querySelectorAll(".buttonDelete");
+let buttonsEdit = document.querySelectorAll(".buttonEdit");
+let buttonsSave = document.querySelectorAll(".buttonSave");
 const details = document.querySelector("#details");
 const name = document.getElementById("name");
 const lastname = document.getElementById("lastName");
@@ -106,20 +107,89 @@ function clearFields() {
   checkValid();
 }
 
+function createUsersTable(usersArray) {
+  let tableRows = `<tr>
+      <th>No.</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Gender</th>
+      <th>Creation Date</th>
+      <th></th>
+    </tr>`;
+  tableRows += usersArray.map(writeDetails);
+  return tableRows;
+}
+
+function writeDetails(user, id) {
+  let usersRows = "";
+  usersRows += user.displayDetails(id);
+  return usersRows;
+}
+
+function updateButtons() {
+  buttonsDelete = document.querySelectorAll(".buttonDelete");
+  buttonsEdit = document.querySelectorAll(".buttonEdit");
+  buttonsSave = document.querySelectorAll(".buttonSave");
+  buttonsDelete.forEach(function(elem) {
+    elem.addEventListener("click", function(e) {
+      e.preventDefault();
+      const idDelete = e.target.id.match(/\d+/g);
+      usersList.delete(idDelete);
+      usersArray = usersList.get();
+      id = 0;
+      details.innerHTML = createUsersTable(usersArray);
+      updateButtons();
+    });
+  });
+
+  for (let i = 0; i < buttonsEdit.length; i++) {
+    buttonsEdit[i].addEventListener("click", function(e) {
+      e.preventDefault();
+      const idEdit = e.target.id.match(/\d+/g);
+      const nameEdit = document.querySelector(`.name${idEdit}`);
+      const lastNameEdit = document.querySelector(`.lastName${idEdit}`);
+      const genderEdit = document.querySelector(`.gender${idEdit}`);
+      let currentButtonEdit = document.querySelector(`#edit${idEdit}`);
+      let currentButtonSave = document.querySelector(`#save${idEdit}`);
+      usersArray = usersList.get();
+      usersArray.map(function(obj) {
+        if (obj.userId == idEdit) {
+          nameEdit.innerHTML = `<input value = ${obj.firstName}>`;
+          lastNameEdit.innerHTML = `<input value = ${obj.lastName}>`;
+          genderEdit.innerHTML = `<input value = ${obj.gender}>`;
+        }
+      });
+      currentButtonEdit.classList.add("hide");
+      currentButtonSave.classList.remove("hide");
+    });
+  }
+
+  for (let i = 0; i < buttonsSave.length; i++) {
+    buttonsSave[i].addEventListener("click", function(e) {
+      e.preventDefault();
+      idEdit = e.target.id.match(/\d+/g);
+      currentButtonEdit = document.querySelector(`#edit${idEdit}`);
+      currentButtonSave = document.querySelector(`#save${idEdit}`);
+      usersList.update(
+        idEdit,
+        document.querySelector(`.name${idEdit} input`).value,
+        document.querySelector(`.lastName${idEdit} input`).value,
+        document.querySelector(`.gender${idEdit} input`).value
+      );
+      usersArray = usersList.get();
+      details.innerHTML = createUsersTable(usersArray);
+      updateButtons();
+      currentButtonEdit.classList.remove("hide");
+      currentButtonSave.classList.add("hide");
+    });
+  }
+}
+
 name.addEventListener("input", checkValid);
 lastname.addEventListener("input", checkValid);
 
 button.addEventListener("click", function(e) {
   e.preventDefault();
-
-  details.innerHTML = `<tr>
-  <th>No.</th>
-  <th>First Name</th>
-  <th>Last Name</th>
-  <th>Gender</th>
-  <th>Creation Date</th>
-  <th></th>
-</tr>`;
   const gender = document.querySelector('input[name="gender"]:checked').value;
   usersArray = usersList.get();
   const user = new User(name.value, lastname.value, gender, usersArray);
@@ -136,76 +206,7 @@ button.addEventListener("click", function(e) {
   }
 
   usersArray = usersList.get();
-  usersArray.map(writeDetails);
-
+  details.innerHTML = createUsersTable(usersArray);
+  updateButtons();
   clearFields();
-
-  // To do:
-  // create an object User
-  // In the <div id="details"> display data from the form
-  // tip: get string with details from user.displayDetails()
 });
-
-// buttonDelete.addEventListener("click", function(e) {
-//   e.preventDefault();
-// });
-
-document.getElementById("details").addEventListener("click", function(event) {
-  const idEdit = event.target.id.match(/\d+/g);
-  if (event.target.className === "buttonDelete") {
-    usersList.delete(event.target.id.match(/\d+/g));
-    usersArray = usersList.get();
-    id = 0;
-    details.innerHTML = `<tr>
-  <th>No.</th>
-  <th>First Name</th>
-  <th>Last Name</th>
-  <th>Gender</th>
-  <th>Creation Date</th>
-  <th></th>
-</tr>`;
-    usersArray.map(writeDetails);
-  }
-  if (event.target.className === "buttonSave") {
-    usersList.update(
-      idEdit,
-      document.querySelector(`.name${idEdit} input`).value,
-      document.querySelector(`.lastName${idEdit} input`).value,
-      document.querySelector(`.gender${idEdit} input`).value
-    );
-    usersArray = usersList.get();
-    details.innerHTML = `<tr>
-  <th>No.</th>
-  <th>First Name</th>
-  <th>Last Name</th>
-  <th>Gender</th>
-  <th>Creation Date</th>
-  <th></th>
-</tr>`;
-    console.log(usersArray);
-    usersArray.map(writeDetails);
-    event.target.classList.remove("buttonSave");
-    event.target.classList.add("buttonEdit");
-  }
-  if (event.target.className === "buttonEdit") {
-    const nameEdit = document.querySelector(`.name${idEdit}`);
-    const lastNameEdit = document.querySelector(`.lastName${idEdit}`);
-    const genderEdit = document.querySelector(`.gender${idEdit}`);
-    const buttonEdit = document.querySelector(`#edit${idEdit}`);
-    usersArray = usersList.get();
-    buttonEdit.innerHTML = "Save";
-    usersArray.map(function(obj) {
-      if (obj.userId == idEdit) {
-        nameEdit.innerHTML = `<input value = ${obj.firstName}>`;
-        lastNameEdit.innerHTML = `<input value = ${obj.lastName}>`;
-        genderEdit.innerHTML = `<input value = ${obj.gender}>`;
-      }
-    });
-    event.target.classList.add("buttonSave");
-    event.target.classList.remove("buttonEdit");
-  }
-});
-
-function writeDetails(user, id) {
-  details.innerHTML += user.displayDetails(id);
-}
